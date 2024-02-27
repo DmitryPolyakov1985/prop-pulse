d4e3b502f0fd7bcebe24cd3c3bfe3dfe589badb8
Dmitry Polyakov <dps.coding@gmail.com>
2024-02-26 23:44:58 -0700

    lots of frontend done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@/assets/styles/globals.css';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -9,7 +11,11 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 };
